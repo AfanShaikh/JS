aefbf2fdc179df120a5f126664e76e4514737c09
AfanShaikh <shaikhaffan.8286@gmail.com>
2025-08-21 10:59:18 +0530

    code of the day

---
+++ switch.js
@@ -0,0 +1,121 @@
+let flag = true;
+
+switch(flag){
+    case true:
+        console.log('true');
+    break;
+    case false:
+        console.log('false');
+    break;
+    default:
+        console.log(nothing);
+}
+
+let week = 2;
+
+switch(week){
+    case 1:
+        console.log('Monday');
+    break;
+    case 2:
+        console.log('Tuesday');
+    break;
+    case 3:
+        console.log('Wednesday');
+    break;
+    case 4:
+        console.log('Thursday');
+    break;
+    case 5:
+        console.log('Friday');
+    break;
+    case 6:
+        console.log('Saturday');
+    break;
+    case 7:
+        console.log('Sunday');
+    break;
+    default:
+        console.log('Holiday');
+}
+
+let vowel = "u";
+
+switch(vowel){
+    case "a":
+        console.log('vowel');
+    break;
+    case "e":
+        console.log('vowel');
+    break;
+    case "i":
+        console.log('vowel');
+    break;
+    case "o":
+        console.log('vowel');
+    break;
+    case "u":
+        console.log('vowel');
+    break;
+    default:
+        console.log('consonant');
+}
+
+let month = 13;
+
+switch(month){
+    case 1:
+        console.log('January');
+    break;
+    case 2:
+        console.log('February');
+    break;
+    case 3:
+        console.log('March');
+    break;
+    case 4:
+        console.log('April');
+    break;
+    case 5:
+        console.log('May');
+    break;
+    case 6:
+        console.log('June');
+    break;
+    case 7:
+        console.log('July');
+    break;
+    case 8:
+        console.log('August');
+    break;
+    case 9:
+        console.log('September');
+    break;
+    case 10:
+        console.log('October');
+    break;
+    case 11:
+        console.log('November');
+    break;
+    case 12:
+        console.log('December');
+    break;   
+    default:
+        console.log('Nothing')
+}
+
+let traffic_ligth = "G";
+
+switch(traffic_ligth){
+    case "R":
+        console.log("Stop");
+    break;
+    case "Y":
+        console.log("Ready");
+    break;
+    case "G":
+        console.log("Go");
+    break;
+    default:
+        console.log("Wait Patiently")
+}
