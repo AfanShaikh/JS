2f4220e69e94a77cf1be2939d564a0c72f21e0ef
AfanShaikh <shaikhaffan.8286@gmail.com>
2025-09-02 11:05:54 +0530

    code of the day

---
+++ array.js
@@ -0,0 +1,16 @@
+//! array[indexing]
+let arr = [[1,2],3,4,[5]];
+let arr1 = ["Afan","Shaikh","Sonu"];
+let arr2 = [{name:"Afan",surname:"Shaikh",nickName:"Sonu"}];
+
+for(let i=0;i<arr.length;i++){
+    console.log(arr[i]);
+}
+
+for(let i=0;i<arr1.length;i++){
+    console.log(arr1[i]);
+}
+
+for(let i=0;i<arr2.length;i++){
+    console.log(arr2[i]);
+}
