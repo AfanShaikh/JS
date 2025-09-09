710ba982e066590242116f62f219e3fd35f1d007
AfanShaikh <shaikhaffan.8286@gmail.com>
2025-09-09 11:04:54 +0530

    code of the day

--- Math.js
+++ Math.js
@@ -24,3 +24,23 @@ console.log('~num5:', Math.floor(num5));
 let num6 = 125;
 console.log(Math.ceil(Math.pow(num6, 1/3)));
 console.log(Math.floor(Math.pow(num6, 1/3)));
+
+//fround
+let num7 = 3.33;
+console.log('~num7:', Math.fround(num7))
+
+//Random 
+let num8 = 6.5;
+console.log('~num8:', Math.round(num8))
+
+function randomNumber(x){
+    console.log('~ randomNumber:', Math.round(Math.random()* x));
+}
+randomNumber(num8);
+
+// Log value
+let num9 = 16;
+let num10 = 1;
+
+console.log(Math.log(num9)/Math.log(num10));
+console.log(Math.log2(num9));
