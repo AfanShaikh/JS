79c850bf46fc2b5481f8ca2215c57bdaffceeac3
AfanShaikh <shaikhaffan.8286@gmail.com>
2025-09-20 11:10:58 +0530

    code of the day

--- Fetch_API/script.js
+++ Fetch_API/script.js
@@ -1,27 +1,33 @@
-const base_url =`https://jsonplaceholder.typicode.com/posts`;
+const base_url = `https://jsonplaceholder.typicode.com/posts`;
 
-const apiCalling = () =>{
-    let data = fetch(base_url);
-    console.log('🚀 ~ data:', data);
+// const apiCalling = () =>{
+//     let data = fetch(base_url);
+//     console.log('🚀 ~ data:', data);
 
-    //handling promise with sync way
-    fetch(base_url)
-      .then((res) => res.json())
-      .then((res) => console.log(res))
-      .catch((err) => console.log(err))
-      .finally(() => console.log("data has been fetch"));
-}
-apiCalling();
+//     //handling promise with sync way
+//     fetch(base_url)
+//       .then((res) => res.json())
+//       .then((res) => console.log(res))
+//       .catch((err) => console.log(err))
+//       .finally(() => console.log("data has been fetch"));
+// }
+// apiCalling();
 
 const apiCall = async () => {
-        //handling promise with async way
-    try{
-        let data = await fetch(base_url);
-        let res = await data.json();
-        console.log('🚀 ~ res:', res);
-    }
-    catch (err){
-        console.log('🚀 ~ err:', err);
-    }
-}
-apiCall();
+  //handling promise with async way
+  try {
+    let data = await fetch(base_url);
+    let res = await data.json();
+    console.log("🚀 ~ res:", res);
+    res.map((els) => {
+    alert(
+    ` body: ${els.body} 
+    id: ${els.id} 
+    title: ${els.title} 
+    userId: ${els.userId}`
+      );
+    });
+  } catch (err) {
+    console.log("🚀 ~ err:", err);
+  }
+};
